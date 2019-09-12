91bbd233c65ff52f1898d9370eac1ba9e8608112
YueLonghuwang <Yueyongchnag95@163.com>
2019-09-12 18:27:51 +0800

    日志打印

--- src/main/java/com/rengu/project/integrationoperations/controller/WebSendToCController.java
+++ src/main/java/com/rengu/project/integrationoperations/controller/WebSendToCController.java
@@ -1,18 +1,25 @@
 package com.rengu.project.integrationoperations.controller;
 
-import com.rengu.project.integrationoperations.configuration.LogConfig;
 import com.rengu.project.integrationoperations.entity.*;
 import com.rengu.project.integrationoperations.enums.SystemStatusCodeEnum;
-import com.rengu.project.integrationoperations.service.SysLogService;
+import com.rengu.project.integrationoperations.repository.HostRepository;
+import com.rengu.project.integrationoperations.service.DynamicJobService;
 import com.rengu.project.integrationoperations.service.WebSendToCService;
 import com.rengu.project.integrationoperations.service.WebReceiveToCService;
 import lombok.NonNull;
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
+import org.quartz.SchedulerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.*;
+import javax.swing.text.html.ImageView;
 import javax.validation.constraints.NotNull;
+import javax.xml.transform.Result;
 import java.io.IOException;
 
 /**
@@ -27,20 +34,21 @@ import java.io.IOException;
 public class WebSendToCController {
     private final WebSendToCService webSendToCService;
     private final WebReceiveToCService receiveInformationService;
-    private final SysLogService sysLogService;
+
     // 设置同批数据为同一序号
     private final int serialNumber = 0;
 
+
+
     @Autowired
-    public WebSendToCController(WebSendToCService webSendToCService, WebReceiveToCService receiveInformationService, SysLogService sysLogService) {
+    public WebSendToCController(WebSendToCService webSendToCService, WebReceiveToCService receiveInformationService) {
         this.webSendToCService = webSendToCService;
         this.receiveInformationService = receiveInformationService;
-        this.sysLogService = sysLogService;
+
     }
 
     // 发送系统校时
     @PostMapping("/sendSystemTiming/communication")
-    @LogConfig("发送系统校时")
     public ResultEntity sendSystemTiming(@NotNull String timeNow, @NotNull String timingPattern, @NonNull String time, @NonNull String host, @NonNull String updateAll) throws IOException {
         try {
             webSendToCService.sendSystemTiming(timeNow, time, timingPattern, host, updateAll, serialNumber);
@@ -50,9 +58,19 @@ public class WebSendToCController {
         return new ResultEntity(SystemStatusCodeEnum.SUCCESS, null);
     }
 
+    //添加定时发送
+    @PostMapping("/addSystemTimingTask/communication")
+    public ResultEntity addSystemTimingTask(  String timeNow,  String timingPattern, String time,  String sendTime,  String host,   String updateAll) throws SchedulerException {
+
+        System.out.println(timeNow+"   2"+time+"   3"+sendTime+"   4"+timingPattern+"   5"+ host+"   6"+ updateAll+"  7 "+ serialNumber);
+
+        return new ResultEntity(SystemStatusCodeEnum.SUCCESS, webSendToCService.addTimeSendTask(timeNow, time,sendTime, timingPattern, host, updateAll, serialNumber));
+    }
+
+
+
     // 设备复位
     @PostMapping("/sendDeviceRestoration/communication")
-    @LogConfig("设备复位")
     public ResultEntity sendDeviceRestoration(String timeNow, String executePattern, String host, @NonNull String updateAll) {
         try {
             webSendToCService.sendDeviceRestoration(timeNow, executePattern, host, updateAll, serialNumber);
@@ -64,7 +82,6 @@ public class WebSendToCController {
 
     // 设备自检指令
     @PostMapping("/sendDeviceCheckCMD/communication")
-    @LogConfig("设备自检指令")
     public ResultEntity sendDeviceCheckCMD(DeviceCheckCMD deviceCheckCMD, String host, @NonNull String updateAll) {
         try {
             webSendToCService.sendDeviceCheckCMD(deviceCheckCMD, host, updateAll, serialNumber);
@@ -76,7 +93,6 @@ public class WebSendToCController {
 
     // 软件版本更新
     @PostMapping("/sendSoftwareUpdateCMD/communication")
-    @LogConfig("软件版本更新")
     public ResultEntity sendSoftwareUpdateCMD(String timeNow, String cmd, String softwareID, String host, @NonNull String updateAll) {
         try {
             webSendToCService.sendSoftwareUpdateCMD(timeNow, cmd, softwareID, host, updateAll, serialNumber);
@@ -88,7 +104,6 @@ public class WebSendToCController {
 
     // 设备网络参数更新指令
     @PostMapping("/sendDeviceNetworkCMD/communication")
-    @LogConfig("设备网络参数更新指令")
     public ResultEntity sendDeviceNetworkCMD(SendDeviceNetWorkParam sendDeviceNetWorkParam, String host, @NonNull String updateAll) {
         try {
             webSendToCService.sendDeviceNetworkCMD(sendDeviceNetWorkParam, host, updateAll, serialNumber);
@@ -100,7 +115,6 @@ public class WebSendToCController {
 
     // 设备工作流程控制指令
     @PostMapping("/sendDeviceWorkFlowCMD/communication")
-    @LogConfig("设备工作流程控制指令")
     public ResultEntity sendDeviceWorkFlowCMD(DeviceWorkFlowCMD deviceWorkFlowCMD, SystemControlCMD systemControlCMD, int count, String host, @NonNull String updateAll) {
         try {
             webSendToCService.sendDeviceWorkFlowCMD(deviceWorkFlowCMD, systemControlCMD, count, host, updateAll, serialNumber);
@@ -112,7 +126,6 @@ public class WebSendToCController {
 
     //设备工作流程控制指令(雷达分机指令)
     @PostMapping("/sendExtensionInstructionsCMD/communication")
-    @LogConfig("设备工作流程控制指令(雷达分机指令)")
     public ResultEntity sendExtensionInstructions(DeviceWorkFlowCMD deviceWorkFlowCMD, int count, String host, String updateAll,int radarExtensionNum) {
         webSendToCService.sendExtensionInstructionsCMD(deviceWorkFlowCMD, count, host, updateAll, serialNumber,radarExtensionNum);
         return new ResultEntity(SystemStatusCodeEnum.SUCCESS, "发送雷达分机指令成功");
@@ -120,11 +133,10 @@ public class WebSendToCController {
 
     //设备工作流程控制指令(雷达系统指令)
     @PostMapping("/sendSystemInstructionsCMD/communication")
-    @LogConfig("设备工作流程控制指令(雷达系统指令)")
     public ResultEntity sendSystemInstructions(SystemControlCMD systemControlCMD, int count, String host, String updateAll,int radarSystemNum) {
         //return new ResultEntity(SystemStatusCodeEnum.SUCCESS, webSendToCService.sendSystemInstructionsCMD(systemControlCMD, count, host, updateAll, serialNumber,radarSystemNum));
         webSendToCService.sendSystemInstructionsCMD(systemControlCMD, count, host, updateAll, serialNumber,radarSystemNum);
-         return new ResultEntity(SystemStatusCodeEnum.SUCCESS, "发送雷达系统指令成功");
+        return new ResultEntity(SystemStatusCodeEnum.SUCCESS, "发送雷达系统指令成功");
     }
 
     // 查询所有
